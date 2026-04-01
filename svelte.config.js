cd29b4727e5bb6892db333123a7956680efbdd81
trongnam13823 <trantrongnam138@gmail.com>
2026-04-01 20:39:53 +0700

    fallback: 'index.html'

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '200.html' // may differ from host to host
+			fallback: 'index.html' // may differ from host to host
 		}),
 		alias: {
 			$: resolve('./src')
